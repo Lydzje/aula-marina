151e20bbc9609cebe50f6c5461e2d394dcff4ce4
Lydzje <to.lydzje@gmail.com>
2019-01-09 11:00:10 +0100

    Página contacto cambiada

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 
     <title>Aula Marina | Contacto</title>
 
-    <link rel="stylesheet" href="css/mainR.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
           crossorigin="anonymous">
@@ -15,17 +15,21 @@
   </head>
 
   <body>
-    <div class="wrapper-contact">
+    <div class="bg bg-contact"></div>
+    <div class="wrapper">
       <?php include "nav.php"?>
-      <div class="title-page">
-        <h1>CONTACTO</h1>
+      <div class="content">
+
+      
+      <div class="title">
+        <span>CONTACTO</span>
       </div>
       <div class="loc-cont-box">
         <div class="location-box">
           <h1>Ubicación</h1>
           <div class="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3193.433893605343!2d-2.4026608850680775!3d36.832083279941955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzbCsDQ5JzU1LjUiTiAywrAyNCcwMS43Ilc!5e0!3m2!1ses!2ses!4v1542824639805"
-                    width="400" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
         </div>
         <div class="contact-box">
@@ -47,6 +51,7 @@
               Laboratorio: planta semisótano, laboratorio S-12.
             </p>
           </div>
+        </div>
         </div>
       </div>
       <?php include "footer.php"?>
