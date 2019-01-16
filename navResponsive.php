66c78a286f4e519244dffc7de9585d6b6c70ed9c
rmt871 <raaau8@gmail.com>
2019-01-16 17:05:28 +0100

    nav responsve

---
+++ navResponsive.php
@@ -0,0 +1,31 @@
+
+<link rel="stylesheet" href="./css/style.css">
+
+<div class="header-resp">
+    <div class="logo-nav-resp">
+        <div class="logo-resp">
+            <a href="./index.php">
+                <img src="./res/aula-marina-logo.png">
+            </a>
+        </div>
+        <button>
+        <i class="fas fa-bars"></i>
+            <div class="nav-resp">
+                <ul>
+                    <a href="index.php"><li>INICIO</li></a>
+                    <a href="species-of-the-month.php"><li>ESPECIE DEL MES</li></a>
+                    <a href="aula-marina.php"><li>AULA MARINA</li></a>
+                    <a href="activities.php"><li>ACTIVIDADES</li></a>
+                    <a href="projects.php"><li>PROYECTOS</li></a>
+                    <a href="news.php"><li>NOTICIAS</li></a>
+                    <a href="contact.php"><li>CONTACTO</li></a>
+                </ul>
+            </div>
+        </button>
+    </div>
+    <div class="lang-resp">
+        <a href="#">ES</a>
+        <span>/</span>
+        <a href="#">EN</a>
+    </div>
+</div>
